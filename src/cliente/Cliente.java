e2ab76979d3b7beec064c90486ab5b688cbc6246
guimaraesrodrigues <guimaraes_rodrigues@yahoo.com>
2017-05-08 01:25:23 -0300

    Alteração de diretórios para utilizar apenas 1 projeto

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import rmi.InterfaceCli;
+import rmi.InterfaceServ;
+
+/**
+ *
+ * @author a1441990
+ */
+public class Cliente {
+    public static void main(String[] args) {
+        try{
+            Registry referenciaServicoNomes = LocateRegistry.getRegistry("localhost", 1099);
+            InterfaceServ server = (InterfaceServ) referenciaServicoNomes.lookup("Servidor");
+            InterfaceCli client = new CliImpl(server);
+            //client.echo("echo - cliente");
+            //server.chamar("chamar - cliente", client);
+        }
+        catch (Exception ex) {
+            System.out.println("Cliente main:");
+            ex.printStackTrace();
+        }
+        
+    }
+}
